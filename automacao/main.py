35313088dd6d2b634e86b5ab558a6f6a5811e78c
d4rk4ngeluz <rfdsouza@hotmail.com>
2023-10-14 00:21:56 -0300

    Projeto refatorado

--- automacao/main.py
+++ automacao/main.py
@@ -20,7 +20,6 @@ sleep(5)
 #ADICIONAR ITEM
 navegador.find_element(By.XPATH, "//*[contains(text(), ' Adicionar')]").click()
 sleep(5)
-
 navegador.find_element(By.ID, "mui-2").send_keys("Bolsa Clássica Jessica Sun")
 sleep(0.5)
 navegador.find_element(By.ID, "mui-3").send_keys("Bolsa Clássica Jessica Sun - Branca")
@@ -39,41 +38,3 @@ sleep(5)
 
 #FECHAR NAVEGADOR
 navegador.quit()
-
-
-
-
-
-# pyautogui.click(x=865, y=725)
-# pyautogui.write("D:\IJJ\Itensdesafio\Tenis_infantil.jpg")
-# pyautogui.press("Enter")
-  
-
-
-# @when("clicar em enviar novo produto")
-# def form_text(context):
-#     btn = context.browser.find_element(By.XPATH, "/html/body/div/header/section[2]/div/div[1]/div/form/button")
-#     btn.submit()
-
-
-# # button = WebDriverWait(navegador, 5).until(EC.visibility_of_element_located((By.XPATH, 
-# # "//*[contains(text(), ' Adicionar')]")))
-# # ActionChains(navegador).move_to_element(button).perform()
-
-# # form_email = navegador.find_element(By.NAME, "email").send_keys("mgeambastiane@gmail.com")
-# # form_body = navegador.find_element(By.NAME, "body").send_keys("Automação com Selenium Web Driver")
-# # form_select = navegador.find_element(By.NAME, "assunto")
-
-
-# # select = Select(form_select)
-
-# # select.select_by_visible_text("Contratar profissionais")
-
-# # form_btn = navegador.find_element(By.XPATH, "/html/body/div[1]/div[2]/section[8]/div[1]/form/button")
-
-# # print(form_btn)
-
-# # form_btn.submit()
-
-# # # resultados.click()
-
---
+++ automacao/objects.py
@@ -0,0 +1,50 @@
+from selenium.webdriver import Firefox
+from selenium.webdriver.common.by import By
+from time import sleep
+
+class Navegador:
+    def __init__(self, url):
+        self.navegador = Firefox()
+        self.navegador.maximize_window()
+        self.url = url
+
+    def acessar_site(self):
+        self.navegador.get(self.url)
+
+    def preencher_formulario(self, email, password):
+        form_email = self.navegador.find_element(By.NAME, "email")
+        form_email.send_keys(email)
+        form_password = self.navegador.find_element(By.NAME, "password")
+        form_password.send_keys(password)
+        self.navegador.find_element(By.XPATH, "/html/body/div/main/form/button").click()
+        sleep(5)
+
+    def adicionar_item(self, nome, descricao, preco, desconto, imagem):
+        self.navegador.find_element(By.XPATH, "//*[contains(text(), ' Adicionar')]").click()
+        sleep(5)
+        self.navegador.find_element(By.ID, "mui-2").send_keys(nome)
+        sleep(0.5)
+        self.navegador.find_element(By.ID, "mui-3").send_keys(descricao)
+        sleep(0.5)
+        self.navegador.find_element(By.XPATH, "/html/body/div/header/section[2]/div/div[1]/div/form/div[3]/div/label[3]").click()
+        sleep(0.5)
+        self.navegador.find_element(By.ID, "mui-4").send_keys(preco)
+        sleep(0.5)
+        self.navegador.find_element(By.ID, "mui-6").send_keys(desconto)
+        sleep(0.5)
+        self.navegador.find_element(By.XPATH, "//*[@id='mui-5']").send_keys(imagem)
+
+    def cadastrar_item(self):
+        self.navegador.find_element(By.XPATH, "/html/body/div/header/section[2]/div/div[1]/div/form/button").submit()
+        sleep(5)
+
+    def fechar_navegador(self):
+        self.navegador.quit()
+
+# Uso da classe
+navegador = Navegador("https://projetofinal.jogajuntoinstituto.org/")
+navegador.acessar_site()
+navegador.preencher_formulario("orion@squadorion.org", "orion1234")
+navegador.adicionar_item("Bolsa Clássica Jessica Sun", "Bolsa Clássica Jessica Sun - Branca", "205,50", "20,00", 'D:\IJJ\Itensdesafio\jessica.jpg')
+navegador.cadastrar_item()
+navegador.fechar_navegador()
